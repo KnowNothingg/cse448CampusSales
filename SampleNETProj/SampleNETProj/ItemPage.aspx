b9850e0eb158ccd1273f942f2b7584750d700c06
Ling Xiang <xiangl2@miamioh.edu>
2019-04-08 15:03:23 -0400

    .Net proj

---
+++ SampleNETProj/SampleNETProj/ItemPage.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ItemPage.aspx.cs" Inherits="SampleNETProj.ItemPage" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <asp:Table ID="tblItems" runat="server" Width ="100%">
+     <asp:TableHeaderRow>
+        <asp:TableHeaderCell>Sold by</asp:TableHeaderCell>
+         <asp:TableHeaderCell>Description</asp:TableHeaderCell>
+        <asp:TableHeaderCell>Price</asp:TableHeaderCell>
+        <asp:TableHeaderCell>options</asp:TableHeaderCell>
+    </asp:TableHeaderRow>
+
+    </asp:Table>
+
+
+
+
+
+
+
+
+
+
+
+
+</asp:Content>
---
+++ SampleNETProj/SampleNETProj/ItemPage.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.AspNet.Identity;
+
+namespace SampleNETProj
+{
+    public partial class ItemPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(! IsPostBack) loadItems(0);
+        }
+
+        private void loadItems(int pageNumber)
+        {
+            TableRow tr;
+            TableCell td;
+           
+            foreach(ds.spGetItemsRow r in (new dsTableAdapters.spGetItemsTableAdapter()).GetData(5, 0))
+            {
+                tr = new TableRow();
+                td = new TableCell();
+                td.Text = r.UserName;
+                tr.Cells.Add(td);
+
+                td = new TableCell();
+                td.Text = r.item_name;
+                tr.Cells.Add(td);
+
+                td = new TableCell();
+                td.Text = "$" + r.price;
+                tr.Cells.Add(td);
+
+                td = new TableCell();
+                if(r.Id.Equals(User.Identity.GetUserId()))
+                {
+                    td.Text = "";
+                } else
+                {
+                    td.Text = "<a href = '#'> Buy </a>";
+                }
+                tr.Cells.Add(td);
+
+                tblItems.Rows.Add(tr);
+            }
+        }
+    }
+}
